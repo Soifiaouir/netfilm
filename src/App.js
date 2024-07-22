bb776f6be5dad690141cfa9dec763a3707d792dc
Soifiaouir <soifiaouir@gmail.com>
2024-07-22 15:54:07 +0200

    routes mise a jours détail

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PopularMovies from './component/api/PolpularMovies';
-import MovieDetails from './page/MovieDetails/MovieDetails';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Netflim</h1>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<PopularMovies />} />
-            <Route path="/movie/:id" element={<MovieDetails />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>Netflim</h1>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
