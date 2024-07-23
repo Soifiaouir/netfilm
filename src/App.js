92fd0927bebd4c87e740b0bb050fe1321bf6fd8a
Brand35 <brandou35000@gmail.com>
2024-07-23 15:12:41 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import PopularMovies from './component/api/PolpularMovies.js';
-import Hero from './component/hero/hero.js';
-import React, { useState, useCallback } from 'react';
-import { Outlet } from 'react-router-dom';
+import Hero from "./component/hero/hero.js";
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
 import SearchBar from "./component/search/search";
 import { SearchProvider } from "./component/searchcontent/SearchContent.jsx";
-import Navbar from './component/menu/Navbar';
-import './App.css';
+import Navbar from "./component/menu/Navbar";
+import "./App.css";
 
 function App() {
-  const [genreFilter, setGenreFilter] = useState('');
+  const [genreFilter, setGenreFilter] = useState("");
 
   const handleFilterChange = (genreId) => {
     console.log("Filter changed to:", genreId);
     setGenreFilter(genreId);
   };
 
-  return(
+  return (
     <div>
-      <Hero/>
-      <SearchProvider>
-        <header className="App">
-              <Hero />
-          <SearchBar />
-          <Navbar onFilterChange={handleFilterChange} />
-        </header>
-        <main>
-        <div className='hero_container'>
-        <Hero/>
-        <div className='search_container'>
-        <SearchBar/>
-        </div>
-        </div>
+      <header className="App">
+        <Navbar onFilterChange={handleFilterChange} />
+      </header>
+      <main>
+        <SearchProvider>
+          <div className="hero_container">
+            <Hero />
+            <div className="search_container">
+              <SearchBar />
+            </div>
+          </div>
           <Outlet context={{ genreFilter }} />
-        </main>
-      </SearchProvider>
+        </SearchProvider>
+      </main>
     </div>
   );
 }
-  
 
 export default App;
